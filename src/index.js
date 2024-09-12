eec344783ddfefa3d81cd5763a4d16c51463f35a
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-09-13 00:54:49 +0300

    test

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import "./styles.css";
 import "./rightbar.css";
 import "./mainside.css";
 
-console.log("biba");
+import { Projects } from "./projects.js";
+import { Tasks } from "./tasks.js";
+
+
+let projectManager = Projects;
+let taskManager = Tasks;
+
+console.log(taskManager.tasksArray)
+console.log(projectManager.projectsArray)
+taskManager.appendTasks();
