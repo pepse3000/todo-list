3b264dbbce7fd87426c70c00d45cb0fe87b91256
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-09-13 19:22:33 +0300

    test

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import "./rightbar.css";
 import "./mainside.css";
 
 import { Projects } from "./projects.js";
-import { Tasks } from "./tasks.js";
+import { ProjectTasks } from "./tasks.js";
 import { DOMSideGenerator } from "./domgenerator.js";
+import { Tags } from "./tags.js";
 
 let projectManager = Projects;
-let taskManager = Tasks;
+let taskManager = ProjectTasks;
+let tagManager = Tags;
 
 DOMSideGenerator.createProjects();
