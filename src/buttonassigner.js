1fa0b0e8695cd3c6b5a8bc6c3413d2af085cc675
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-09-14 23:23:15 +0300

    add assign functions to mainside buttons

--- src/buttonassigner.js
+++ src/buttonassigner.js
@@ -3,6 +3,8 @@ import { Tasks } from "./tasks.js";
 import { Tags } from "./tags.js";
 import { DOMSideGenerator } from "./domgenerator.js";
 import { PageLocator } from "./pagelocator.js";
+import { ObjectCreator } from "./utils/objectcreator.js";
+import { DomUpdater } from "./utils/domupdater.js";
 
 export const ButtonAssigner = (function() {
     const assignProjectButtons = function() {
@@ -17,8 +19,60 @@ export const ButtonAssigner = (function() {
         let allBtns = document.querySelectorAll(".side-btn");
 
         allBtns[0].addEventListener("click", PageLocator.openTodayPage);
-        console.log(allBtns)
+        allBtns[1].addEventListener("click", PageLocator.openNext7DaysPage);
+        allBtns[2].addEventListener("click", PageLocator.openInboxPage);
+        allBtns[3].addEventListener("click", PageLocator.openActivityPage);
+        allBtns[4].addEventListener("click", PageLocator.openProjectsPage);
+        
     }
 
-    return { assignMenuButtons }
+    const assignShowSetInfo = function() {
+        let showBtn = document.querySelector("#show-set-info");
+
+        showBtn.addEventListener("click", () => PageLocator.showSetInfo());
+    }
+
+    const assingSetInfoDayPickers = function() {
+        let todayBtn = document.querySelector("#torday");
+        let tmrwBtn = document.querySelector("#tmrw");
+
+        todayBtn.addEventListener("click", () => ObjectCreator.changeCalendarDay("today"));
+        tmrwBtn.addEventListener("click", () => ObjectCreator.changeCalendarDay("tmrw"));
+    }
+
+    const assignCreateNewTask = function() {
+        let form = document.querySelector(".new-task")
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            let name = document.querySelector("#taskname").value;
+            let priority = document.querySelector("input[name='priority']:checked").value;
+
+            let tags = document.querySelectorAll("input[type='checkbox']:checked");
+            let tagsNames = [];
+            tags.forEach(tag => {
+                tagsNames.push(tag.name);
+            })
+            
+            let date = document.querySelector("#date").value;
+            
+            Tasks.createTask(
+                Tasks.tasksArray,
+                "",
+                name,
+                tagsNames,
+                priority,
+                date,
+                "open"
+            )
+
+            let taskName = document.querySelector("#taskname");
+            taskName.value = "";
+            
+            PageLocator.showSetInfo("open");
+            DomUpdater.updateTodayList();
+        })
+    }
+
+    return { assignMenuButtons, assignShowSetInfo, assingSetInfoDayPickers, assignCreateNewTask }
 })();
