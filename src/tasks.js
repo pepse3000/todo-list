33416e38cdfce6b824ab8bc46655b689f8483255
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-09-18 22:31:03 +0300

    add condtions for page locator, add functions: undoLastCompletedTask, getCompletedTasksByProject, getLastTaskActivity, getTaskState

--- src/tasks.js
+++ src/tasks.js
@@ -1,5 +1,6 @@
 import { Projects } from "./projects.js";
 import { TimeConverter } from "./utils/timeconverter.js";
+import { Tags } from "./tags.js";
 
 export const Tasks = (function() {
     let tasksArray = [];
@@ -29,11 +30,6 @@ export const Tasks = (function() {
     ) {
         let newTask = { ...taskTemplate };
 
-        if (appendProjectId) {
-            newTask.appendProjectName = projectArray[appendProjectId - 1]["projectName"];
-            newTask.appendProjectId = appendProjectId;
-        }
-
         newTask.taskName = taskName;
         newTask.priority = priority;
         newTask.dueDate = dueDate;
@@ -42,20 +38,27 @@ export const Tasks = (function() {
         newTask.createDate = new Date();
 
         newTask.taskId = array.length + 1; 
-    
+
+        if (appendProjectId) {
+            Projects.projectsArray.find(project => project.projectId == appendProjectId).assignedTasks.push(newTask.taskId)
+            newTask.appendProjectName = projectArray[appendProjectId - 1]["projectName"];
+            newTask.appendProjectId = appendProjectId;
+            Projects.saveToLocalStorage();
+        }
+
         array.push(newTask);
         saveToLocalStorage();
     }
 
-    const appendTasksFirstLoad = function() {
-        tasksArray.forEach(task => {
-            let project = projectArray.find(proj => proj.projectId === task.appendProjectId);
+    // const appendTasksFirstLoad = function() {
+    //     tasksArray.forEach(task => {
+    //         let project = projectArray.find(proj => proj.projectId === task.appendProjectId);
 
-            if (project && !project.assignedTasks.includes(task)) {
-                project.assignedTasks.push(task);
-            }
-        });
-    };
+    //         if (project && !project.assignedTasks.includes(task)) {
+    //             project.assignedTasks.push(task);
+    //         }
+    //     });
+    // };
 
     const saveToLocalStorage = function() {
         localStorage.setItem("tasksArray", JSON.stringify(tasksArray));
@@ -66,7 +69,17 @@ export const Tasks = (function() {
             return tasksArray.filter(task => task.status == "done" && new Date(task.completeDate).getDate() == new Date().getDate());
         }
 
+        if (status == "7days") {
+            return tasksArray
+            .filter(task => task.status == "done")
+            .filter(task => new Date(task.completeDate).getDate() < new Date().getDate() + 7 && new Date(task.completeDate).getMonth() <= new Date().getMonth() && new Date(task.completeDate).getDate() < new Date().getDate() + 7)
+        }
+
         return tasksArray.filter(task => task.status == "done");
+    }
+
+    const getCompletedTasksByProject = function(projectId) {
+        return tasksArray.filter(task => task.appendProjectId == projectId && task.status == "done");
     }
 
     const getUncompletedTasks = function(status) {
@@ -74,6 +87,16 @@ export const Tasks = (function() {
             return tasksArray
             .filter(task => task.status != "done")
             .filter(task => new Date(task.dueDate).getDate() <= new Date().getDate() && new Date(task.dueDate).getMonth() <= new Date().getMonth())
+        }
+
+        if (status == "7days") {
+            return tasksArray
+            .filter(task => task.status != "done")
+            .filter(task => new Date(task.dueDate).getDate() > new Date().getDate() && new Date(task.dueDate).getMonth() <= new Date().getMonth() && new Date(task.dueDate).getDate() < new Date().getDate() + 7)
+        }
+
+        if (status == "inbox") {
+            return tasksArray.filter(task => !task.appendProjectName && task.status == "open");
         }
 
         return tasksArray.filter(task => task.status == "open" || task.status == "in_progress");
@@ -84,6 +107,26 @@ export const Tasks = (function() {
             createTask(
                 tasksArray,          
                 1,                      
+                "Set up multiplayer",    
+                ["Selfcare"], 
+                "medium",                          
+                TimeConverter.convertStringToDate("tomorrow"),            
+                "in_progress"             
+            );
+
+            createTask(
+                tasksArray,          
+                2,                      
+                "Set up multiplayer",    
+                ["Selfcare"], 
+                "medium",                          
+                TimeConverter.convertStringToDate("tomorrow"),            
+                "in_progress"             
+            );
+
+            createTask(
+                tasksArray,          
+                2,                      
                 "Set up multiplayer",    
                 ["Selfcare"], 
                 "medium",                          
@@ -107,20 +150,17 @@ export const Tasks = (function() {
                 "Pay $1 to Odin Project",    
                 ["Finance"], 
                 "medium",                          
-                TimeConverter.convertStringToDate("2024-09-10"),             
+                "2024-09-10",             
                 "open"             
             );
 
-            appendTasksFirstLoad();
         } else {
             tasksArray = JSON.parse(localStorage.getItem("tasksArray"));
-            appendTasksFirstLoad();
         }
     })();
 
     const completeTask = function(taskElement) {
         let taskId = "";
-        console.log(taskElement);
 
         if (!taskElement.parentElement.id) {
             taskId = taskElement.parentElement.parentElement.id;
@@ -133,23 +173,80 @@ export const Tasks = (function() {
         });
 
         let taskDom = document.querySelectorAll(`#${taskId}`);
-        taskDom.forEach(task => task.classList.add("checked"));
+        taskDom.forEach(task => task.classList.add("checked", "hide"));
         
         task.status = "done";
         task.completeDate = new Date();
 
-        console.log(task)
         saveToLocalStorage();
     }
 
+    const undoLastCompletedTask = function(taskElement) {
+        if (taskElement) {
+            let taskId = "";
+
+            if (!taskElement.parentElement.id) {
+                taskId = taskElement.parentElement.parentElement.id;
+            } else {
+                taskId = taskElement.parentElement.id;
+            }
+    
+            let task = tasksArray.find(task => {
+                return +task.taskId == +taskId.slice(4);
+            });
+
+            let taskDom = document.querySelectorAll(`#${taskId}`);
+            taskDom.forEach(task => task.classList.remove("checked", "hide"));
+
+            task.status = "open";
+            task.completeDate = "";
+    
+            saveToLocalStorage();
+            return;
+        }
+
+        let completedTasks = tasksArray
+        .filter(task => task.status === "done")
+        .sort((a, b) => new Date(a.completeDate) - new Date(b.completeDate)).reverse();
+
+        let taskId = completedTasks[0].taskId;
+
+        tasksArray.forEach(task => { 
+            if (+task.taskId === +taskId) {
+                 task.status = "open" 
+                 task.completeDate = "";
+                }
+            });
+
+        saveToLocalStorage();
+    }
+
+    const getLastTaskActivity = function(includeDate) {
+        if (tasksArray.filter(task => task.completeDate).length == 0) {
+            return [];
+        }
+
+        if (!includeDate) {
+            return tasksArray.filter(task => task.status == "done").sort((a, b) => new Date(b.completeDate) - new Date(a.completeDate))[0];
+        }
+
+        return tasksArray.filter(task => task.status == "done").sort((a, b) => new Date(b.completeDate) - new Date(a.completeDate))[0].completeDate;
+    }
+
+    const getTaskState = function(taskId) {
+        return tasksArray.filter(task => task.taskId == +taskId)[0].status;
+    }
     return { 
         tasksArray, 
-        createFirstLoadTasks, 
-        appendTasksFirstLoad, 
+        createFirstLoadTasks,  
         createTask, 
         saveToLocalStorage,
         getCompletedTasks,
         getUncompletedTasks,
         completeTask,
+        undoLastCompletedTask,
+        getCompletedTasksByProject,
+        getLastTaskActivity,
+        getTaskState
     }
 })();
