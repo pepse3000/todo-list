321a009e7ebdf43981a3d886652f4b99f1e1a3ff
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-09-18 22:27:35 +0300

    small test / fixes

--- src/index.js
+++ src/index.js
@@ -4,21 +4,20 @@ import "./mainside.css";
 import "./createDivs.css";
 
 import { Projects } from "./projects.js";
+import { Tags } from "./tags.js";
 import { Tasks } from "./tasks.js";
 import { DOMSideGenerator } from "./domgenerator.js";
 import { PageLocator } from "./pagelocator.js";
 import { ButtonAssigner } from "./buttonassigner.js";
-import { Tags } from "./tags.js";
 import { ObjectCreator } from "./utils/objectcreator.js";
-
-let projectManager = Projects;
-let taskManager = Tasks;
-let tagManager = Tags;
+import { DomUpdater } from "./utils/domupdater.js";
 
 DOMSideGenerator.createProjects();
 DOMSideGenerator.createTags();
 DOMSideGenerator.createTodos();
-PageLocator.openTodayPage();
+// PageLocator.openTodayPage();
 ButtonAssigner.assignMenuButtons();
 ButtonAssigner.assignCompleteTask();
+ButtonAssigner.assignUndoCompletedTask();
 ObjectCreator.createNewTagInput();
+
