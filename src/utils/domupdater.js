eebce3543d4234e6d6f49a4db30d6eba63467237
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-09-18 22:31:20 +0300

    domupdater small fixes

--- src/utils/domupdater.js
+++ src/utils/domupdater.js
@@ -1,19 +1,92 @@
 import { Tasks } from "../tasks";
 import { Tags } from "../tags.js";
+import { Projects } from "../projects.js";
 import { ObjectCreator } from "./objectcreator.js";
 import { DOMSideGenerator } from "../domgenerator.js";
 import { ButtonAssigner } from "../buttonassigner.js";
+import { TimeConverter } from "./timeconverter.js";
 
 export const DomUpdater = (function() {
-    const updateTodayList = function () {
+    const updateTodayList = function (status) {
         let todosListMain = document.querySelector(".todos-list");
         let todosListSide = document.querySelector(".todos-container");
 
-        if (todosListMain.parentElement.classList.contains("days7page")) {
+        if (todosListMain.parentElement && todosListMain.parentElement.classList.contains("days7page")) {
             todosListMain.innerHTML = "";
             todosListMain.appendChild(ObjectCreator.createTodayList("7days"));
+            todosListSide.innerHTML = "";
+
+            let infoBars = document.querySelectorAll(".info");
+            infoBars[0].textContent = Tasks.getUncompletedTasks("7days").length;
+            infoBars[1].textContent = Tasks.getCompletedTasks("7days").length;
+
             DOMSideGenerator.createTodos();
             ButtonAssigner.assignCompleteTask();
+            DOMSideGenerator.createProjects();
+            return;
+        }
+
+        if (todosListMain.parentElement.classList.contains("inboxpage")) {
+            todosListMain.innerHTML = "";
+            todosListMain.appendChild(ObjectCreator.createTodayList("inbox"));
+            todosListSide.innerHTML = "";
+
+            let infoBars = document.querySelectorAll(".info");
+            infoBars[0].textContent = Tasks.getUncompletedTasks("inbox").length;
+            infoBars[1].textContent = Tasks.getCompletedTasks().length;
+            
+            DOMSideGenerator.createTodos();
+            ButtonAssigner.assignCompleteTask();
+            DOMSideGenerator.createProjects();
+            return;
+        }
+
+        if (todosListMain.parentElement.classList.contains("activitypage")) {
+            todosListMain.innerHTML = "";
+            todosListMain.appendChild(ObjectCreator.createCompleteTasks());
+            todosListSide.innerHTML = "";
+
+            let infoBars = document.querySelectorAll(".info");
+            console.log(TimeConverter.getHourAndMinute(Tasks.getLastTaskActivity(true)));
+
+            DOMSideGenerator.createTodos();
+            ButtonAssigner.assignCompleteTask();
+            ButtonAssigner.assignUndoActivityTask();
+
+            infoBars[0].textContent = String(Tasks.getLastTaskActivity(true)).length > 0 ? TimeConverter.getHourAndMinute(Tasks.getLastTaskActivity(true)) : "No activity";
+            infoBars[1].textContent = Tasks.getCompletedTasks().length;
+            DOMSideGenerator.createProjects();
+            return;
+        } 
+        
+        if (todosListMain.parentElement.classList.contains("project-todos")) {
+            todosListMain.innerHTML = "";
+            todosListMain.appendChild(ObjectCreator.createTodayList("project"));
+            todosListSide.innerHTML = "";
+            DOMSideGenerator.createTodos();
+
+            ButtonAssigner.assignCompleteTask();
+            ButtonAssigner.assignUndoActivityTask();
+            DOMSideGenerator.createProjects();
+            return;
+        } 
+
+        if (todosListMain.parentElement.classList.contains("single-project-todos")) {
+            todosListMain.innerHTML = "";
+            let projectName = document.querySelector(".project-name").textContent;
+            let project = Projects.projectsArray.find(project => project.projectName == projectName);
+            let projectId = project.projectId;
+            todosListMain.appendChild(ObjectCreator.createProjectTasks(projectId));
+            todosListSide.innerHTML = "";
+            DOMSideGenerator.createTodos();
+
+            let infoBars = document.querySelectorAll(".info");
+            infoBars[1].textContent = Tasks.getCompletedTasksByProject(projectId).length;
+            infoBars[2].textContent = project["assignedTasks"].length;
+
+            ButtonAssigner.assignCompleteTask();
+            ButtonAssigner.assignUndoActivityTask();
+            DOMSideGenerator.createProjects();
             return;
         }
 
@@ -27,6 +100,7 @@ export const DomUpdater = (function() {
         infoBars[0].textContent = Tasks.getUncompletedTasks("today").length;
         infoBars[1].textContent = Tasks.getCompletedTasks("today").length;
 
+        DOMSideGenerator.createProjects();
         updateTagsList();
         ButtonAssigner.assignCompleteTask();
     }
@@ -51,7 +125,14 @@ export const DomUpdater = (function() {
             tagName.textContent = tag["tagName"];
 
             tagNumber.classList.add("number");
-            tagNumber.textContent = tag["appendedProjects"].length + tag["appendedTasks"].length;
+
+            let count = 0;
+            Tasks.tasksArray.forEach(task => {
+                if (tag["appendedTasks"].includes(task["taskId"]) && task.status != "done") { count++ }
+            })
+
+            console.log(count);
+            tagNumber.textContent = count;
 
             tagElement.appendChild(imgHolder);
             tagElement.appendChild(tagName);
@@ -60,26 +141,28 @@ export const DomUpdater = (function() {
             tagsContainer.appendChild(tagElement);
         })
 
-        let tagsAssigner = document.querySelector(".tags-assigner");
-        tagsAssigner.innerHTML = "";
-
-        Tags.tagsArray.forEach(tag => {
-            let tagElement = document.createElement("input");
-            let tagLabel = document.createElement("label");
-
-            tagElement.type = "checkbox";
-            tagElement.id = tag.tagName;
-            tagElement.name = tag.tagName;
-            tagElement.value = tag.tagId;
-
-            tagLabel.htmlFor = tag.tagName;
-            tagLabel.textContent = tag.tagName;
-            tagLabel.style.background = `${tag.color}20`;
-            tagLabel.style.color = tag.color;
-
-            tagsAssigner.appendChild(tagElement);
-            tagsAssigner.appendChild(tagLabel);
-        })
+        if (document.querySelector(".tags-assigner")) {
+            let tagsAssigner = document.querySelector(".tags-assigner");
+            tagsAssigner.innerHTML = "";
+    
+            Tags.tagsArray.forEach(tag => {
+                let tagElement = document.createElement("input");
+                let tagLabel = document.createElement("label");
+    
+                tagElement.type = "checkbox";
+                tagElement.id = tag.tagName;
+                tagElement.name = tag.tagName;
+                tagElement.value = tag.tagId;
+    
+                tagLabel.htmlFor = tag.tagName;
+                tagLabel.textContent = tag.tagName;
+                tagLabel.style.background = `${tag.color}20`;
+                tagLabel.style.color = tag.color;
+    
+                tagsAssigner.appendChild(tagElement);
+                tagsAssigner.appendChild(tagLabel);
+            })
+        }
 
         Tags.saveToLocalStorage();
     }
